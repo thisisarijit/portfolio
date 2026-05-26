5d062dcdfd69e657b7a213b9d0e8fbb80ce21371
Arijit Karmakar <arijitkarmakar873@gmail.com>
2026-05-26 18:02:09 +0530

    implement the theme button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
-    <div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-
-    </div>
-  )
-}
-
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import ThemeToggleButton from "../components/ThemeToggleButton";
 
 const Home = () => {
-  return <div></div>;
+  return <div className="min-h-screen bg-background">
+    {/* Theme */}
+    <ThemeToggleButton />
+    
+    {/* navbar */}
+
+    {/* M A I N */}
+    
+    {/* F O O T E R */}
+
+  </div>;
 };
 
 export default Home;
