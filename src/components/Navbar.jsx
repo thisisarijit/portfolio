4bff33ddf039c7015023457488c92af804d522da
Arijit Karmakar <arijitkarmakar873@gmail.com>
2026-05-27 00:53:40 +0530

    implement navbar for desktop and menu button for small screen

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { cn } from "../lib/utils";
+import { Menu, X } from "lucide-react";
+
+const navItems = [
+  { name: "Home", href: "#hero" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "#contact" },
+];
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <nav
+      className={cn(
+        "fixed w-full z-40 transition-all duration-300",
+        isScrolled
+          ? "py-3 bg-background/20 backdrop-blur-md shadow-md"
+          : "py-5",
+      )}
+    >
+      <div className="container text-primary flex items-center justify-around">
+        {/* L O G O */}
+        <a href="#home" className="text-2xl font-extrabold flex items-center">
+          <span className="relative z-10">
+            <span className="text-foreground">ARIJIT </span> KARMAKAR
+          </span>
+        </a>
+
+        {/* nav links */}
+        {/* D E S K T O P */}
+        <div className="border-2 rounded-xl py-2 px-5 bg-card hidden md:flex space-x-8">
+          {navItems.map((item, index) => (
+            <a
+              href={item.href}
+              key={index}
+              className="text-foreground hover:text-primary-hover transition-all duration-300"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+
+        {/* M O B I L E */}
+        <button
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+          className="md:hidden p-2 text-foreground z-50"
+          aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
+        >
+          {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+        <div
+          className={cn(
+            "fixed inset-0 bg-background/10 backdrop-blur-md z-40 flex flex-col justify-center items-center",
+            "transition-all duration-300 md:hidden",
+            isMenuOpen
+              ? "opacity-100 pointer-events-auto"
+              : "opacity-0 pointer-events-none",
+          )}
+        >
+          <div className="flex flex-col space-y-8 text-xl">
+            {navItems.map((item, key) => (
+              <a
+                key={key}
+                href={item.href}
+                className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import ThemeToggleButton from "../components/ThemeToggleButton";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-  return <div className="min-h-screen bg-background">
-    {/* Theme */}
-    <ThemeToggleButton />
-    
-    {/* navbar */}
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme */}
+      <ThemeToggleButton />
+      {/* navbar */}
+      <Navbar />
 
-    {/* M A I N */}
-    
-    {/* F O O T E R */}
+      {/* M A I N */}
 
-  </div>;
+      {/* F O O T E R */}
+    </div>
+  );
 };
 
 export default Home;
